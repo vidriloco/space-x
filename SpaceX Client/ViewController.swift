c088f5dccb92d168a4284cd6d7f9bf923db31e5a
Alejandro Cruz Paz <alejandrop@backbase.com>
2021-10-27 23:40:25 +0200

    Initial Commit

---
+++ SpaceX Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX Client
+//
+//  Created by Alejandro Cruz on 27/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
